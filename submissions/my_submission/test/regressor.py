9819312898511608bf77173498501d08e8d19304
lnatale6 <natale.leonardo1997@gmail.com>
2019-10-25 14:36:06 +0200

    Changes to folder structure

---
+++ submissions/my_submission/test/regressor.py
@@ -0,0 +1,13 @@
+from sklearn.neural_network import MLPRegressor
+from sklearn.base import BaseEstimator
+
+
+class Regressor(BaseEstimator):
+    def __init__(self):
+	    self.reg = MLPRegressor(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+
+    def fit(self, X, y):
+        self.reg.fit(X, y)
+
+    def predict(self, X):
+        return self.reg.predict(X)
